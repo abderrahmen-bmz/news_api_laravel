2884bc0840a7c00e4d5e99d557e5ea240f7a1043
AbderrahmenBmz <abdogray@gmail.com>
2020-05-07 01:28:04 +0100

    add API Token

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory( \App\User::class , 30 )->create();
+        factory( \App\Category::class , 10 )->create();
+        factory( \App\Post::class , 400 )->create();
+        factory( \App\Comment::class , 900 )->create();
     }
 }
